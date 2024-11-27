51870f71c550a0d4fda7606a58d29a8926d4e183
Braden Fiedel <u1307785@utah.edu>
2024-11-26 19:45:47 -0700

    Add unit test for search service with Mockito mocking

--- lib/services/search_service.dart
+++ lib/services/search_service.dart
@@ -3,9 +3,13 @@ import 'package:bookworms_app/models/BookSummary.dart';
 import 'package:http/http.dart' as http;
 
 class SearchService {
+  final http.Client client;
+
+  SearchService({http.Client? client}) : client = client ?? http.Client();
+
   // Retrieve the book summaries of the given query from the server.
   Future<List<BookSummary>> getBookSummaries(String query, int resultLength) async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:5247/search/title?query=$query'));
+    final response = await client.get(Uri.parse('http://10.0.2.2:5247/search/title?query=$query'));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       final List<BookSummary> bookSummaries = [];
---
+++ test/get_book_summaries_test.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'package:bookworms_app/models/BookSummary.dart';
+import 'package:bookworms_app/services/search_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'mocks/get_book_summaries_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  group('SearchService', () {
+    late SearchService searchService;
+    late MockClient mockClient;
+
+    setUp(() {
+      mockClient = MockClient();
+      searchService = SearchService(client: mockClient);
+    });
+
+    test('returns a list of BookSummaries if the http call completes successfully', () async {
+      final mockResponse = jsonEncode([
+        {
+          'bookId': '1',
+          'title': 'Book 1',
+          'authors': ['Author 1'],
+          'difficulty': 'easy',
+          'rating': 1.0
+        },
+        {
+          'bookId': '2',
+          'title': 'Book 2',
+          'authors': ['Author 2'],
+          'difficulty': 'medium',
+          'rating': 2.0
+        }
+      ]);
+      
+      when(mockClient.get(Uri.parse('http://10.0.2.2:5247/search/title?query=test')))
+          .thenAnswer((_) async => http.Response(mockResponse, 200));
+
+      final result = await searchService.getBookSummaries('test', 2);
+
+      expect(result, isA<List<BookSummary>>());
+      expect(result.length, 2);
+
+      expect(result[0].id, '1');
+      expect(result[0].title, 'Book 1');
+      expect(result[0].authors, ['Author 1']);
+      expect(result[0].difficulty, 'easy');
+      expect(result[0].rating, 1.0);
+
+      expect(result[1].id, '2');
+      expect(result[1].title, 'Book 2');
+      expect(result[1].authors, ['Author 2']);
+      expect(result[1].difficulty, 'medium');
+      expect(result[1].rating, 2.0);
+    });
+
+    test('throws an exception if the http call fails', () async {
+      when(mockClient.get(Uri.parse('http://10.0.2.2:5247/search/title?query=test')))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(() async => await searchService.getBookSummaries('test', 2), throwsException);
+    });
+  });
+}
