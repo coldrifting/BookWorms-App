83ae08fde6f518617a2b17ccc04d0d084a316191
JosieFiedel <112005344+JosieFiedel@users.noreply.github.com>
2024-12-03 18:40:40 -0700

    merge into prototype

--- lib/home/bookshelf_widget.dart
+++ lib/home/bookshelf_widget.dart
@@ -1,4 +1,4 @@
-import 'package:bookworms_app/models/BookSummary.dart';
+import 'package:bookworms_app/models/book_summary.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -22,13 +22,13 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView(
         children: [
           const SizedBox(height: 16),
-          const BookshelfWidget(name: "Recommended", images: [Demo.image1, Demo.image2, Demo.image3, Demo.image4], books: [Demo.book1, Demo.book2, Demo.book3, Demo.book4]),
+          BookshelfWidget(name: "Recommended", images: [Demo.image1, Demo.image2, Demo.image3, Demo.image4], books: [Demo.book1, Demo.book2, Demo.book3, Demo.book4]),
           const SizedBox(height: 24),
           _progressTracker(),
           const SizedBox(height: 24),
-          const BookshelfWidget(name: "Animals", images: [Demo.image2, Demo.image5, Demo.image6, Demo.image7], books: [Demo.book2, Demo.book5, Demo.book6, Demo.book7]),
+          BookshelfWidget(name: "Animals", images: [Demo.image2, Demo.image5, Demo.image6, Demo.image7], books: [Demo.book2, Demo.book5, Demo.book6, Demo.book7]),
           const SizedBox(height: 24),
-          const BookshelfWidget(name: "Fairytales", images: [Demo.image8, Demo.image9, Demo.image7, Demo.image10], books: [Demo.book8, Demo.book9, Demo.book7, Demo.book10]),
+          BookshelfWidget(name: "Fairytales", images: [Demo.image8, Demo.image9, Demo.image7, Demo.image10], books: [Demo.book8, Demo.book9, Demo.book7, Demo.book10]),
           const SizedBox(height: 16),
         ],
       ),
