91ef3c9de6bc0f3b70ab5fd602c2aafcd5fbe2fd
JosieFiedel <112005344+JosieFiedel@users.noreply.github.com>
2025-04-03 20:04:09 -0600

    Add universal dependency

--- lib/resources/network.dart
+++ lib/resources/network.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:universal_io/io.dart' show Platform;
 
 final String serverBaseUri = getServerAddress();
 
